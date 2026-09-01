9c424186885a7c91fbc096c4c1aeb04091847434
Luisa Fernanda T <luisafernandatapiabarraza84@gmail.com>
2026-08-31 21:41:19 -0500

    Preparar backend para Aiven y Render

--- Backend/src/config/conexion-bd.js
+++ Backend/src/config/conexion-bd.js
@@ -5,12 +5,17 @@ dotenv.config();
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
-  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT),
+  connectionLimit: Number(process.env.DB_CONNECTION_LIMIT) || 10,
   queueLimit: 0,
 });
 
